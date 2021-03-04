75ab99a3d6ae3b3d71217f43f50aa9eb8e9d1dcc
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2021-03-04 11:38:05 +0100

    Add checkRoundWinner function

--- src/components/GameWrap/GameWrap.js
+++ src/components/GameWrap/GameWrap.js
@@ -5,16 +5,46 @@ import './GameWrap.scss';
 function GameWrap({ user }) {
   const [value, setValue] = useState('');
   const [valueComp, setValueComp] = useState('');
+  const [points, setPoints] = useState(0);
+  const [pointsComp, setPointsComp] = useState(0);
+  const [winner, setWinner] = useState('');
 
+  const computer = 'Computer';
   const computerChoice = ['Kamień', 'Papier', 'Nożyczki'];
 
-  const getValuComputer = () => {
-    setValueComp(computerChoice[Math.floor(Math.random() * 3)]);
+  let valueC = computerChoice[Math.floor(Math.random() * 3)];
+
+  const getValueComputer = () => {
+    setValueComp(valueC);
+  };
+
+  const checkRoundWinner = (e) => {
+    let winnerIs = user;
+    let value = e.target.value;
+
+    if (value === valueC) {
+      winnerIs = 0;
+    } else if (
+      (valueC === 'Kamień' && value === 'Nożyczki') ||
+      (valueC === 'Nożyczki' && value === 'Papier') ||
+      (valueC === 'Papier' && value === 'Kamień')
+    ) {
+      winnerIs = computer;
+    }
+
+    if (user === winnerIs) {
+      setPoints(points + 1);
+      setWinner(`tą partię wygrał ${user}`);
+    } else if (computer === winnerIs) {
+      setPointsComp(pointsComp + 1);
+      setWinner(`tą partię wygrał ${computer}`);
+    }
   };
 
   const getValueHandler = (e) => {
     setValue(e.target.value);
-    getValuComputer();
+    getValueComputer();
+    checkRoundWinner(e);
   };
 
   return (
@@ -43,15 +73,16 @@ function GameWrap({ user }) {
         <div className='user__box'>
           <div className='user'>{user}</div>
           <div className='choice'>{value}</div>
-          <div className='points'>0</div>
+          <div className='points'>{points}</div>
         </div>
         <span>VS</span>
         <div className='user__box'>
-          <div className='user'>Computer</div>
+          <div className='user'>{computer}</div>
           <div className='choice'>{valueComp}</div>
-          <div className='points'>0</div>
+          <div className='points'>{pointsComp}</div>
         </div>
       </div>
+      <small>{winner}</small>
     </div>
   );
 }
